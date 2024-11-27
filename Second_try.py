7134f3f7be2c4b2cad72a60024d60e146c3883fd
opilane <opilane@HP-CZC2349HTP>
2024-11-27 16:44:31 +0200

    Round 2

--- Second_try.py
+++ Second_try.py
@@ -1,46 +1,45 @@
 ﻿from datetime import *
 from calendar import *
 from math import *
-# from socket import RCVALL_SOCKETLEVELONLY 
-
-
-# #Ülesanne 1
-# paevadekogus=(monthrange(2024,11)[1]) #calendar modulist
-# print(paevadekogus)
-
-# tana=date.today() #nimetus()-finktsioon
-# tanaf=date.today().strftime("%B %d, %Y")
-
-# print(f"Tere! Tana on {tanaf}")
-# d=tana.day #nimetus - omadus
-# m=tana.month
-# y=tana.year
-# print(d)
-# print(m)
-# print(y)
-
-# detsP=monthrange(2024,12)[1] #31
-
-# novP=monthrange(2024,11)[1] #30
-# jaak=detsP+novP-d
-# jaak2=novP-d
-# print(f"Aasta loppuni on {jaak}")
-# print(f"Kuu loppuni on {jaak2}")
-
-# #Ülesanne 2
-# vastus1=3 + 8 / (4 - 2) * 4
-# vastus2=3+8 / 4 - 2 * 4
-# vastus3= (3 + 8) / (4 - 2) * 4
-# print(vastus1)
-# print(vastus2)
-# print(vastus3)
-# print(vastus1,"\n",vastus2,"\n", vastus3)
-
-#Ülesanne 3
-from math import *
 from random import *
 from xml.sax import default_parser_list
 
+# from socket import RCVALL_SOCKETLEVELONLY 
+
+
+#Ülesanne 1
+paevadekogus=(monthrange(2024,11)[1]) #calendar modulist
+print(paevadekogus)
+
+tana=date.today() #nimetus()-finktsioon
+tanaf=date.today().strftime("%B %d, %Y")
+
+print(f"Tere! Tana on {tanaf}")
+d=tana.day #nimetus - omadus
+m=tana.month
+y=tana.year
+print(d)
+print(m)
+print(y)
+
+detsP=monthrange(2024,12)[1] #31
+
+novP=monthrange(2024,11)[1] #30
+jaak=detsP+novP-d
+jaak2=novP-d
+print(f"Aasta loppuni on {jaak}")
+print(f"Kuu loppuni on {jaak2}")
+
+#Ülesanne 2
+vastus1=3 + 8 / (4 - 2) * 4
+vastus2=3+8 / 4 - 2 * 4
+vastus3= (3 + 8) / (4 - 2) * 4
+print(vastus1)
+print(vastus2)
+print(vastus3)
+print(vastus1,"\n",vastus2,"\n", vastus3)
+
+#Ülesanne 3
 try:
     R=float(input("Sisesta R: ")) #с десятичными 1.01
     Sk=pi*R**2 #** степень
@@ -67,4 +66,4 @@ maa=6378 #maa radius km
 maa*=100000 #Radius sm + maa=maa*10000 = maa*=10000
 Lmaa=2*pi*maa
 kogus=int(Lmaa/d)
-print(f"On vaja {kogus} mundi.\nMeil on vaja{kogus*2} eur")
+print(f"On vaja {kogus:,d} mundi.\nMeil on vaja{kogus*2:,d} eur")
